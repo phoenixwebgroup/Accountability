c0379ee1ebe824956e3dd9f8167350c30c822c23
Wesley McClure <wmcclure@phoenixwebgroup.com>
2011-07-19 22:01:37 -0500

    rake file

---
+++ Rakefile.rb
@@ -0,0 +1,36 @@
+require 'rake'
+require 'albacore'
+
+$projectSolution = 'Accountability.sln'
+$artifactsPath = "build"
+$nugetFeedPath = ENV["NuGetDevFeed"]
+$srcPath = File.expand_path('src')
+
+task :teamcity => [:build_release]
+
+task :build => [:build_release]
+
+msbuild :build_release => [:clean, :dep] do |msb|
+  msb.properties :configuration => :Release
+  msb.targets :Build
+  msb.solution = $projectSolution
+end
+
+task :clean do
+    puts "Cleaning"
+    FileUtils.rm_rf $artifactsPath
+	bins = FileList[File.join($srcPath, "**/bin")].map{|f| File.expand_path(f)}
+	bins.each do |file|
+		sh %Q{rmdir /S /Q "#{file}"}
+		#sh 
+    end
+end
+
+desc "Setup dependencies for nuget packages"
+task :dep do
+	package_folder = File.expand_path('Packages')
+    packages = FileList["**/packages.config"].map{|f| File.expand_path(f)}
+	packages.each do |file|
+		sh %Q{nuget install #{file} /OutputDirectory #{package_folder}}
+    end
+end
