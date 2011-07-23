2fe50ab033f23049933e8f0d457404837b7990af
Ryan Ohs <rohs@phoenixwebgroup.com>
2011-07-22 20:27:20 -0500

    override html attribute encoding behavior in .net 4.0

---
+++ src/Accountability/WebApp/HtmlAttributeEncodingNot.cs
@@ -0,0 +1,13 @@
+﻿namespace Accountability.WebApp
+{
+	using System.IO;
+	using System.Web.Util;
+
+	public class HtmlAttributeEncodingNot : HttpEncoder
+	{
+		protected override void HtmlAttributeEncode(string value, TextWriter output)
+		{
+			output.Write(value.Replace("\"", "&quot;"));
+		}
+	}
+}
