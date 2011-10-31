e09d5a9c3a658e5b462d4b5b8a66c3d1a3391ea3
Wesley McClure <wes.mcclure@gmail.com>
2011-10-31 14:29:54 -0500

    Extension methods to access cache and clear method.

---
+++ src/Accountability/CacheExtensions.cs
@@ -0,0 +1,49 @@
+﻿namespace Accountability
+{
+	using System;
+	using Metrics;
+	using MongoDB.Bson;
+	using Users;
+
+	public static class CacheExtensions
+	{
+		public static Lazy<MetricsById> MetricsById = new Lazy<MetricsById>(() => new MetricsById());
+		public static Lazy<SourcesById> SourcesById = new Lazy<SourcesById>(() => new SourcesById());
+
+		public static Metric GetMetric(ObjectId metricId)
+		{
+			return MetricsById.Value[metricId];
+		}
+
+		public static string GetMetricName(ObjectId metricId)
+		{
+			var metric = MetricsById.Value[metricId];
+			if (metric == null)
+			{
+				return "Invalid metric id: " + metricId;
+			}
+			return metric.Name;
+		}
+
+		public static Source GetSource(ObjectId sourceId)
+		{
+			return SourcesById.Value[sourceId];
+		}
+
+		public static string GetSourceName(ObjectId sourceId)
+		{
+			var source = SourcesById.Value[sourceId];
+			if (source == null)
+			{
+				return "Invalid source id: " + sourceId;
+			}
+			return source.Name;
+		}
+
+		public static void ClearAll()
+		{
+			MetricsById.Value.ClearAll();
+			SourcesById.Value.ClearAll();
+		}
+	}
+}
