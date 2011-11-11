0d547506df396302f940d1e15fe190485e4a7f91
ryanohs <rohs@phxwg.com>
2011-11-11 16:15:06 -0600

    Switch to using htmltags model for MetricForm.

--- src/Accountability/Home/MetricForm.cs
+++ src/Accountability/Home/MetricForm.cs
@@ -1,9 +1,12 @@
 namespace Accountability.Home
 {
     using System;
+    using Authentication;
     using HtmlTags;
     using HtmlTags.Extensions;
+    using HtmlTags.UI;
     using Infrastructure;
+    using Metrics;
 
     public class MetricForm
     {
@@ -21,15 +24,15 @@ namespace Accountability.Home
         {
             var table = Tags.Table;
             var row = table.AddBodyRow();
-            row.Cell("Target");
+            row.Cell("Target:");
             row.Cell().Nest(Tags.Span.DataBind("text: Target"));
 
             row = table.AddBodyRow();
-            row.Cell("Source");
+            row.Cell("Source:");
             row.Cell().Nest(Tags.Span.DataBind("text: Source"));
 
             row = table.AddBodyRow();
-            row.Cell("Metric");
+            row.Cell("Metric:");
             row.Cell().Nest(Tags.Span.DataBind("text: Metric"));
 
             div.Nest(table);
@@ -37,17 +40,66 @@ namespace Accountability.Home
 
         private void AddReviewDates(HtmlTag div)
         {
-            throw new NotImplementedException();
+            // TODO Add review dates template
         }
 
         private void AddFeedback(HtmlTag div)
         {
-            throw new NotImplementedException();
+            var table = Tags.Table.Caption("Feedback");
+            var headerRow = table.AddHeaderRow();
+            headerRow.Cell("Date");
+            headerRow.Cell("From");
+            headerRow.Cell("Rating");
+            headerRow.Cell("Notes");
+            headerRow.Cell();
+
+            var addRow = new TableRowTag();
+            addRow.AddClass("top");
+            addRow.Cell().Nest(Tags.Span.DataBind("text: Date"));
+            addRow.Cell().Nest(Tags.Span.DataBind("text: Source"));
+            addRow.Cell().Nest(new GiveFeedback().InputFor(x => x.Rating).DataBind("value: Rating"));
+            addRow.Cell().Nest(new HtmlTag("textarea").DataBind("value: Notes"));
+            addRow.Cell().Nest(
+                Tags.Link.Href("#").Text("Save").DataBind("click: function() { $parent.saveFeedback(); }"));
+            var addBody = new HtmlTag("tbody").DataBind("with: NewFeedback");
+            addBody.Nest(addRow);
+
+            var noteRow = new TableRowTag();
+            noteRow.AddClass("note");
+            noteRow.Cell().Nest(Tags.Span.DataBind("text: Date"));
+            noteRow.Cell().Nest(Tags.Span.DataBind("text: Source"));
+            noteRow.Cell().Nest(Tags.Span.DataBind("text: Rating"));
+            noteRow.Cell().Nest(Tags.Span.DataBind("text: Notes"));
+            noteRow.Cell();
+            var notesBody = new HtmlTag("tbody").DataBind("foreach: Feedback");
+            notesBody.Nest(noteRow);
+
+            table.Nest(
+                addBody,
+                notesBody
+                );
+
+            table.AddClasses("report", "no-hover");
+            div.Nest(table);
         }
 
         private void AddActionItems(HtmlTag div)
         {
-            throw new NotImplementedException();
+            // TODO Add action items template
+        }
+
+        public string GetBlankFeedback()
+        {
+            var json = new
+                           {
+                               Source = UserPrincipal.Current.User.Name,
+                               SourceId = UserPrincipal.Current.User.Id.ToString(),
+                               Date = DateTime.Today,
+                               Notes = "",
+                               Rating = ""
+                           };
+            return string.Format("function getBlankFeedback(){{ return ko.mapping.fromJS({0}); }}",
+                                 Serialize.Javascript(json));
         }
     }
 }
