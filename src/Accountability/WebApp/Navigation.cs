b38bd6f8165a4de4b7e552b2a9893b814b6c1f0d
Wesley McClure <wes.mcclure@gmail.com>
2011-11-21 15:27:37 -0600

    Add admin link (start of a simple menu) -Fix reference to knockout for build

---
+++ src/Accountability/WebApp/Navigation.cs
@@ -0,0 +1,50 @@
+﻿namespace Accountability.WebApp
+{
+	using Authentication;
+	using HtmlTags;
+	using HtmlTags.Constants;
+	using HtmlTags.Extensions;
+	using Users;
+
+	public class Navigation
+	{
+		public static HtmlTag GetHeader()
+		{
+			var header = Tags.Div.AddClass("NavigationHeader");
+			AddTitle(header);
+			AddLogout(header);
+			AddAdminLink(header);
+			return header;
+		}
+
+		private static void AddLogout(HtmlTag header)
+		{
+			var logout = Tags.Link.Attr(HtmlAttributeConstants.Href, "authentication/logout")
+				.Text("(logout)");
+
+			header.Nest(logout);
+		}
+
+		private static void AddTitle(HtmlTag header)
+		{
+			var currentUser = UserPrincipal.Current.User;
+			var userName = currentUser != null ? currentUser.Name : string.Empty;
+
+			var title = Tags.SpanText("Feedback - " + userName);
+			header
+				.Nest(title);
+		}
+
+		private static void AddAdminLink(HtmlTag header)
+		{
+			var currentUser = UserPrincipal.Current.User;
+			var isAdmin = currentUser != null && currentUser.Role.HasFlag(Roles.Admin);
+			if (isAdmin)
+			{
+				var admin = Tags.Link.Attr(HtmlAttributeConstants.Href, "admin/index")
+					.Text("(admin)");
+				header.Nest(admin);
+			}
+		}
+	}
+}
