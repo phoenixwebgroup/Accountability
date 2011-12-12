af81f5269b9760e190864b9314b6a4986f330064
ryanohs <rohs@phxwg.com>
2011-12-12 14:57:31 -0600

    Allow everyone to see project feedback.

--- src/Accountability/Home/AccountabilityEventExtensions.cs
+++ src/Accountability/Home/AccountabilityEventExtensions.cs
@@ -1,16 +1,20 @@
 ﻿namespace Accountability.Home
 {
+	using System.Collections.Generic;
 	using System.Linq;
 	using Authentication;
 	using Metrics;
+	using Mongos;
 
 	public static class AccountabilityEventExtensions
 	{
-		public static IQueryable<AccountabilityEvent> RestrictEventsForCurrentUser(this IQueryable<AccountabilityEvent> source)
+		public static IEnumerable<AccountabilityEvent> RestrictEventsForCurrentUser(this IQueryable<AccountabilityEvent> source)
 		{
 			var currentUserId = UserPrincipal.Current.User.Id;
+			var projectIds = Mongo.Projects.FindAll().Select(x => x.Id).ToArray();
 			return source
-				.Where(e => e.TargetId == currentUserId || e.SourceId == currentUserId);
+				.ToList()
+				.Where(e => e.TargetId == currentUserId || e.SourceId == currentUserId || projectIds.Contains(e.TargetId));
 		}
 	}
 }
